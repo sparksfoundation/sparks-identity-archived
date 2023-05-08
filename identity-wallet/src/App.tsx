e016eaf93f276cb91c8df7387d58aa61c6342295
Paul Pomerleau <paul.m.pomerleau@gmail.com>
2023-05-08 02:55:33 -0400

    init-account

--- identity-wallet/src/App.tsx
+++ identity-wallet/src/App.tsx
@@ -1,18 +1,35 @@
-import sparksLogo from '/images/sparks.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import IdentityProvider from './modules/identity/provider'
+import Privelege from './modules/routing/Privelege'
+import Welcome from './pages/Welcome'
+import Unlock from './pages/Unlock'
+import Create from './pages/Create'
+import Profile from './pages/Profile'
 
-function App() {
+function Routing() {
+  const location = useLocation()
+  const redirects = {
+    welcome: location?.state?.from || '/',
+    profile: location?.state?.from || '/profile',
+    unlock: location?.state?.from || '/unlock-account',
+  }
+
   return (
-    <>
-      <div>
-        <img width="120" height="120" src={sparksLogo} className="logo react" alt="React logo" />
-      </div>
-      <h1>SPARKS</h1>
-      <p>
-        coming soon™
-      </p>
-    </>
+    <Routes>
+      <Route element={<Privelege element={Welcome} registeredTo={redirects.unlock} authenticatedTo={redirects.profile} />} path="/" />
+      <Route element={<Privelege element={Create} registeredTo={redirects.unlock} authenticatedTo={redirects.profile} />} path="/create-account" />
+      <Route element={<Privelege element={Unlock} guestTo={redirects.welcome} authenticatedTo={redirects.profile} />} path="/unlock-account" />
+      <Route element={<Privelege element={Profile} registeredTo={redirects.unlock} guestTo={redirects.welcome} />} path="/profile" />
+    </Routes>
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <BrowserRouter>
+      <IdentityProvider>
+        <Routing />
+      </IdentityProvider>
+    </BrowserRouter>
+  )
+}
---
+++ identity-wallet/src/pages/Welcome.tsx
@@ -0,0 +1,20 @@
+import { Main, H1, Logo, Button, P } from '../modules/components/elements/Primitives'
+
+function App() {
+  return (
+    <Main>
+      <Logo className='mb-4' height={100} width={100} />
+      <H1 className='mb-6'>SPARKS ID</H1>
+      <P className='text-justify mb-6'>
+        Creating a SPARKS Identity allows you to store and control digital 
+        information about yourself, unlocking value without giving up your 
+        privacy. Your identity is your right and creating one is entirely 
+        free. Take control of your digital self today and click below to 
+        get started.
+      </P>
+      <Button href='create-account'>get started</Button>
+    </Main>
+  )
+}
+
+export default App
