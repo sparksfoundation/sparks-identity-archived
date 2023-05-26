8be20caeddd58b3f0378bc19517b625e99cd5892
Makis Otman <makisotman@gmail.com>
2023-05-26 15:33:27 +0300

    Re-add `@ts-ignore`
    See: https://github.com/sparksfoundation/sparks-identity/pull/16#issuecomment-1564296560

--- identity-wallet/src/common/views/apps/apps.tsx
+++ identity-wallet/src/common/views/apps/apps.tsx
@@ -1,3 +1,4 @@
+// @ts-ignore
 import { CockPit } from "@components/layout/cockpit";
 import * as services from '@features/apps';
 
--- identity-wallet/src/features/apps/components/sparks-foundation.tsx
+++ identity-wallet/src/features/apps/components/sparks-foundation.tsx
@@ -2,6 +2,7 @@ import { H5, P } from "@components/elements"
 import { Card } from "@components/elements/card"
 import sparksfoundation from '../assets/covers/sparksfoundation.png'
 import { Button } from "@components/elements/buttons"
+// @ts-ignore
 import { connect } from "@features/channels"
 import { useUser } from "@stores/user"
 import { useState } from "react"
