e3f2fa1da516ad0b675b64cc9e64cc9817005cab
Makis Otman <makisotman@gmail.com>
2023-06-02 15:15:03 +0300

    Additional type changes

--- libs/agent/src/index.ts
+++ libs/agent/src/index.ts
@@ -31,26 +31,26 @@ export type Event = {
   nextKeys?: string[];
   configuration: object | object[] | string[];
 };
-export type InceptionEvent = Event & {};
-export type RotationEvent = Event & {};
-export type DeletionEvent = Event & {};
+export type InceptionEvent = Event & { inceptionId: string }; // the additional objects are examples of what some additional fields could be, still figuring this out
+export type RotationEvent = Event & { rotationId: string };
+export type DeletionEvent = Event & { deletionId: string };
 export type KeyEvents = InceptionEvent | RotationEvent | DeletionEvent;
 
-export type AttestationEvent = {};
-export type RevocationEvent = {};
+export type AttestationEvent = { attestationId: string };
+export type RevocationEvent = { revocationId: string };
 export type CredentialEvents = AttestationEvent | RevocationEvent;
 
-export type DisclosureEvent = {};
-export type SelectiveDiscloureEvent = {};
-export type PredicateEvent = {};
+export type DisclosureEvent = { dId: string };
+export type SelectiveDiscloureEvent = { sDId: string };
+export type PredicateEvent = { pId: string };
 export type VerificationEvents =
   | DisclosureEvent
   | SelectiveDiscloureEvent
   | PredicateEvent;
 
-export type PaymentIssuedEvent = {};
-export type PaymentRecievedEvent = {};
-export type PaymentSettledEvent = {};
+export type PaymentIssuedEvent = { piId: string };
+export type PaymentRecievedEvent = { prId: string };
+export type PaymentSettledEvent = { psId: string };
 export type TransactionEvents =
   | PaymentIssuedEvent
   | PaymentRecievedEvent
@@ -125,11 +125,11 @@ class Identity {
     });
   }
 
-  get identifier() {
+  get identifier(): string {
     return this.#identifier;
   }
 
-  get publicKeys() {
+  get publicKeys(): PublicKeys {
     return {
       encryption: this.#keyPairs.encryption.publicKey,
       signing: this.#keyPairs.signing.publicKey,
